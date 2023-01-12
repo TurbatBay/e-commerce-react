02b1580c7cf1a06d6a3007daf9d52f2f6ba259cf
Morning <morning@mstars-lab2-14s-iMac.local>
2023-01-12 12:34:48 +0800

    styled Single page a bit

--- e-commerce/src/components/SinglePage.jsx
+++ e-commerce/src/components/SinglePage.jsx
@@ -23,30 +23,35 @@ function SinglePage(props) {
                 </div>
             </div>
             <div className="single-right">
-                <p>{props.title}</p>
-                <p>{props.price}0</p>
-                <div>
+                <p className="single-right-title">{props.title}</p>
+                <p className="single-right-price">{props.price}</p>
+                <div className="single-right-review">
                 <ReactStars
                 count={5}
                 onChange={ratingChanged}
-                size={40}
+                size={30}
                 isHalf={true}
                 emptyIcon={<i className="far fa-star"></i>}
                 halfIcon={<i className="fa fa-star-half-alt"></i>}
                 fullIcon={<i className="fa fa-star"></i>}
                 activeColor="#ffd700"
               />
-                    <p>No review</p>
+                    <p className="single-right-review-text">No review</p>
                 </div>
-                <p>Availlability <span> <Check/> In stock </span></p>
+                <p className="single-right-available-p">Availlability <span className="single-right-available-span"> <Check/> In stock </span></p>
                 {/* {props.stock === 0 && <div className="team-card-badge">SOLD OUT</div>} */}
-                <p>Hurry up! only {props.stock} product left in stock</p>
+                <p className="single-right-hurry-p">Hurry up! only {props.stock} product left in stock</p>
                 
                 <hr />
                 <br />
-                <p>Colors: {props.color} <Circle/> <Circle/> </p>
-                <p>Size: {props.size}</p>
-                <p>Quantity</p>
+                <div className="single-right-h3 ">
+                    <h5>Colors: </h5>
+                    <p>{props.color}</p>
+                    <input type="radio" />
+                    <input type="radio" />
+                </div>
+                <p >Size: {props.size}</p>
+                <p className="single-right-h3 ">Quantity</p>
                 <div>
                     <Buttons/>
                     <Heart/>
