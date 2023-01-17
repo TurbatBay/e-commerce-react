504cb6e947b05af595af0e55ac1fd7d4e5883c0a
Morning <morning@mstars-lab2-14s-iMac.local>
2023-01-17 09:02:27 +0800

    Solved white screen problem when using Browser Router reason was i didnt install react-router-dom in the inner

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -4,17 +4,16 @@ import "react-alice-carousel/lib/alice-carousel.css";
 import Footer from "./components/Footer";
 import MainMenu from "./components/MainMenu";
 import { Route, Routes , Link} from "react-router-dom";
-// import About from "./About";
-// import SignIn from "./SignIn";
-// import Signup from "./SignUp";
+import SignIn from "./SignIn";
+import Signup from "./SignUp";
 // import BookList from "./pages/BookList";
 // import Books from "./pages/Books";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login"
-// import Register from "./pages/Register"
-// import Form from "./pages/Form"
-// import NotFound from "./pages/NotFound"
-// import About from "./About";
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Form from "./pages/Form"
+import NotFound from "./pages/NotFound"
+import About from "./About";
 
 function App() {
   return (
@@ -24,35 +23,8 @@ function App() {
         <NavBar />
       </div>
 
-      <MainMenu /> 
+      {/* <MainMenu />  */}
 
-
-      <div className="footer section">
-        <Footer />
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
-{
-  /* <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/signIn" element={<SignIn />} />
-      </Routes> */
-}
-
-{
-  /* <div className="single-page-section section">{singleData}</div> */
-}
-
-{
-  
-}
-
-
-{/* <h1>Day 43-React router</h1>
       <div className="navbar">
         <Link to="./about">About</Link>
         <Link to="./login">login</Link>
@@ -68,4 +40,29 @@ export default App;
         <Route path="/form" element={<Form />} />
         <Route path="/" element={<Home />} />
         <Route path="/*" element={<NotFound />} />
-      </Routes> */}
+      </Routes>
+      {/* <div className="footer section">
+        <Footer />
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
+
+{
+     <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/signIn" element={<SignIn />} />
+      </Routes> 
+}
+
+{
+  /* <div className="single-page-section section">{singleData}</div> */
+}
+
+{
+  
+}
+
+
